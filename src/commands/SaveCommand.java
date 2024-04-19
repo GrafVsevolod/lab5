fc907995f82ac6db2e5384e314b1edc85882d525
GrafVsevolod <166128980+GrafVsevolod@users.noreply.github.com>
2024-04-19 13:16:02 +0300

    Update SaveCommand.java

--- src/commands/SaveCommand.java
+++ src/commands/SaveCommand.java
@@ -1,5 +1,10 @@
 package commands;
-
+/**
+ *  сохраняет коллекцию в файл
+ *
+ * @author GrafVsevolod
+ * @since 1.0
+ */
 public class SaveCommand implements Command {
     @Override
     public void execute(String[] args) throws Exception{
