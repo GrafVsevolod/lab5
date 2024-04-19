3b301d6b0b4704b9f394942dfa7ff18aa3ae6aee
GrafVsevolod <166128980+GrafVsevolod@users.noreply.github.com>
2024-04-19 13:07:09 +0300

    Update ExitCommand.java

--- src/commands/ExitCommand.java
+++ src/commands/ExitCommand.java
@@ -1,5 +1,10 @@
 package commands;
-
+/**
+ * завершает программу (без сохранения в файл)
+ *
+ * @author GrafVsevolod
+ * @since 1.0
+ */
 public class ExitCommand implements Command {
     @Override
     public void execute(String[] args){
