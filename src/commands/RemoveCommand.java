b631c7211d664e3dfa47f9b0f617f891431deb07
GrafVsevolod <166128980+GrafVsevolod@users.noreply.github.com>
2024-04-19 13:13:49 +0300

    Update RemoveCommand.java

--- src/commands/RemoveCommand.java
+++ src/commands/RemoveCommand.java
@@ -4,7 +4,12 @@ import managers.CollectionManager;
 import models.Organization;
 
 import java.util.LinkedList;
-
+/**
+ *  удаляет элемент из коллекции по его id
+ *
+ * @author GrafVsevolod
+ * @since 1.0
+ */
 public class RemoveCommand implements Command {
     @Override
     public void execute(String[] args){
