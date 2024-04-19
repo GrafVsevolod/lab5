55c610963c3a04f0c76a6e5045901ad6610f8bf9
GrafVsevolod <166128980+GrafVsevolod@users.noreply.github.com>
2024-04-19 13:05:50 +0300

    Update ClearCommand.java

--- src/commands/ClearCommand.java
+++ src/commands/ClearCommand.java
@@ -1,7 +1,12 @@
 package commands;
 
 import managers.CollectionManager;
-
+/**
+ * очистить коллекцию
+ *
+ * @author GrafVsevolod
+ * @since 1.0
+ */
 public class ClearCommand implements Command {
     @Override
     public void execute(String[] args){
